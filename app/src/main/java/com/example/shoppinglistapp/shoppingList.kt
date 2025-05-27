4de554758de0df77047773df3a30643e9c65a2c8
nishant <nishantpriya.scorpion@gmail.com>
2025-05-27 18:15:56 +0530

    dialog customisation

--- app/src/main/java/com/example/shoppinglistapp/shoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/shoppingList.kt
@@ -2,13 +2,16 @@ package com.example.shoppinglistapp
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,33 +21,68 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 
-data class shoppingItem(val id:Int, var name:String, var quantity:Int, var isediting:Boolean)
-
+data class shoppingItem(val id: Int, var name: String, var quantity: Int, var isediting: Boolean)
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun shoppingListApp(){
+fun shoppingListApp() {
     var stiems by remember { mutableStateOf(listOf<shoppingItem>()) }
     var showdialog by remember { mutableStateOf(false) }
-
+    var itemname by remember { mutableStateOf("") }
+    var itemquant by remember { mutableStateOf(0) }
 
 
 
     Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
-        Button(onClick = { showdialog=true }, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+        Button(
+            onClick = { showdialog = true },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
             Text("Add Item")
         }
         LazyColumn(
-            modifier = Modifier.fillMaxSize().padding(16.dp)
-        ){
-            items(stiems){}
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            items(stiems) {}
         }
     }
 
-    if(showdialog){
-        AlertDialog(onDismissRequest = {showdialog = false}, confirmButton = {}, title = { Text("Add shoppin items") }, text = { })
+    if (showdialog) {
+        AlertDialog(onDismissRequest = { showdialog = false }, confirmButton = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                
+            }
+
+        }, title = { Text("Add shoppin items") }, text = {
+            OutlinedTextField(
+                value = itemname,
+                onValueChange = { itemname = it },
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            OutlinedTextField(
+                value = itemquant.toString(),
+                onValueChange = { itemquant = it.toInt() },
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+
+
+        })
     }
 }
